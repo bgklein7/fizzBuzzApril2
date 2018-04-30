cddbc35194ed831c4a25d0cd81be6fd254169cb2
unknown <bgklein@outlook.com>
2018-04-30 15:30:08 -0400

    first commit

---
+++ fizzbuzzapril2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace fizzbuzzapril2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int row=0; row<10; row++)
+            {
+                for(int column=0; column<row+1; column++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
